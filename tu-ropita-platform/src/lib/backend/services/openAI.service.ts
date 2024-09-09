a6c283dae7aa29f45ad593bd10c26e2fdbdac485
tom <tcerdeira@itba.edu.ar>
2024-09-08 21:01:33 -0300

    aux: rev impl of other OpenAI service

--- tu-ropita-platform/src/lib/backend/services/openAI.service.ts
+++ tu-ropita-platform/src/lib/backend/services/openAI.service.ts
@@ -1,6 +1,6 @@
-import OpenAI from "openai";
+import { IAIService } from "@/lib/backend/services/interfaces/AI.service.interface";
 import settings from "@/lib/settings";
-import {IAIService} from "@/lib/backend/services/interfaces/AI.service.interface";
+import OpenAI from "openai";
 
 class OpenAIService implements IAIService{
     openai = new OpenAI({
@@ -49,3 +49,80 @@ class OpenAIService implements IAIService{
 }
 
 export const openAIService : IAIService = new OpenAIService();
+
+
+
+// TODO: rev esta impl de servicio de OpenAI
+// import OpenAI from 'openai';
+// import logger from '../../../features/logger/logger';
+// import { EnvironmentVariables } from "../../../shared/EnvironmentVariables";
+
+// class OpenAiService {
+
+//     private logger = logger.child({ label: this.constructor.name });
+    
+//     private apiKey: string;
+//     private openai: OpenAI;
+
+//     constructor() {
+//         this.apiKey = EnvironmentVariables.get().external_services.open_ai.api_key;
+//         this.openai = new OpenAI({ apiKey: this.apiKey });
+//     }
+
+//     async runAssistant(assistant_id: string, prompt: string): Promise<string> {
+
+//         try {
+//             this.logger.info(`Running OpenAI assistant. [assistant_id=${assistant_id}]`);
+//             const thread = await this.openai.beta.threads.create();
+//             await this.openai.beta.threads.messages.create(thread.id, {
+//                 role: "user",
+//                 content: prompt
+//             });
+
+//             const run = await this.openai.beta.threads.runs.create(thread.id, {
+//                 assistant_id: assistant_id,
+//             });
+//             let runStatus = await this.openai.beta.threads.runs.retrieve(thread.id, run.id);
+//             while (runStatus.status !== 'completed') {
+//                 await new Promise(resolve => setTimeout(resolve, 1000));
+//                 runStatus = await this.openai.beta.threads.runs.retrieve(thread.id, run.id);
+//                 this.logger.info(`Assistant run status: ${runStatus.status}`);
+//             }
+
+//             const messages = await this.openai.beta.threads.messages.list(thread.id);
+//             const assistantMessages = messages.data.filter(message => message.role === 'assistant');
+//             const lastMessage = assistantMessages[assistantMessages.length - 1];
+//             if (lastMessage && lastMessage.content.length > 0) {
+//                 const content = lastMessage.content[0];
+//                 if (content.type === 'text') {
+//                     return content.text.value;
+//                 } else {
+//                     throw new Error("The assistant's response was not in text format.");
+//                 }
+//             } else {
+//                 throw new Error("No response from the assistant.");
+//             }
+
+//         } catch (error) {
+//             this.logger.error(`Error using OpenAI Assistants API: ${error}`);
+//             throw error;
+//         }
+//     }
+//     async getEmbedding(text: string): Promise<number[]> {
+//         try {
+//            const embedding = await this.openai.embeddings.create(
+//                {
+//                    input: text,
+//                    model: "text-embedding-ada-002"
+//                }
+//            )
+//            return embedding.data[0].embedding;
+//         } catch (error) {
+//             this.logger.error(`Error using OpenAI Embeddings API: ${error}`);
+//             throw error;
+//         }
+
+//     }
+// }
+
+// export const openAiService = new OpenAiService();
