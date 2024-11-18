e18b1874ec03fdc9ce549fcc1ce9d4a3e0e61066
Santiago Garcia Montanger <sangarcia@itba.edu.ar>
2024-11-18 11:12:05 -0300

    fix : add default categories migraiton

---
+++ tu-ropita-platform/src/lib/backend/migrations/down/15-add-default-categories.sql
@@ -0,0 +1,2 @@
+DELETE FROM public.category
+WHERE id IN (1, 2, 3, 4, 5, 6, 7, 8);
---
+++ tu-ropita-platform/src/lib/backend/migrations/up/15-add-default-categories.sql
@@ -0,0 +1,9 @@
+insert into public.category (id, name)
+values  (1, 'color'),
+        (2, 'style'),
+        (3, 'product_type'),
+        (4, 'size'),
+        (5, 'material'),
+        (6, 'pattern'),
+        (7, 'season'),
+        (8, 'brand');
