21fe0462424dbf70e0585258a417adc4c45f59e7
tomcerdeira <tomas.cerdeira@gmail.com>
2024-12-07 13:34:19 -0300

    Update Footer.tsx

--- tu-ropita-platform/src/components/Footer.tsx
+++ tu-ropita-platform/src/components/Footer.tsx
@@ -1,17 +1,10 @@
-import { Separator } from "@/components/ui/separator";
-
 const Footer = () => {
     return (
       <footer className="bg-background text-foreground p-8 mt-auto hidden md:block">
         <div className="container mx-auto">
           <div className="flex flex-col md:flex-row justify-between items-center">
-            <p className="text-sm">&copy; {new Date().getFullYear()} FindClo. Todos los derechos reservados.</p>
-            <Separator className="my-4 md:hidden" />
-            <nav className="flex space-x-4">
-              <a href="#" className="text-sm hover:underline">Terminos</a>
-              <a href="#" className="text-sm hover:underline">Privacidad</a>
-              <a href="#" className="text-sm hover:underline">Contacto</a>
-            </nav>
+            <p className="text-sm text-foreground/40">Todos los derechos reservados.</p>
+            <p className="text-sm text-foreground/40">FindClo &copy; {new Date().getFullYear()}</p>
           </div>
         </div>
       </footer>
