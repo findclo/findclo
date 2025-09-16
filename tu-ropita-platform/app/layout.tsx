fb0da30f6caa9b811bdcf36671492d554bfe2ab2
Santiago Garcia Montagner <sangarcia@iba.edu.ar>
2025-09-16 20:39:53 -0300

    Add speed insights

--- tu-ropita-platform/app/layout.tsx
+++ tu-ropita-platform/app/layout.tsx
@@ -1,5 +1,6 @@
 import ServerUserProvider from '@/providers/ServerUserProvider';
 import './globals.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,6 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ServerUserProvider>
           {children}
         </ServerUserProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
